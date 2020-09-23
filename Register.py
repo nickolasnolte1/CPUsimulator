d05ad937c3da963c14fdc26d717460e2e4275919
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2020-09-22 23:27:44 -0600

    implements self

--- Register.py
+++ Register.py
@@ -1,6 +1,17 @@
 import IntegratedCircuit
 
-class Register (IntegratedCircuit):
+class Registers():
 
-    def __init__(self):
-        pass
+        def __init__ (self, r0, r1, r2):
+        self.r0 = r0
+        self.r1 = r1
+        self.r3 = r3
+
+        def register0(self):
+            self.r0 = []
+
+        def register1(self):
+            self.r1 = []
+
+        def register2(self):
+            self.r2 = []
