b31f3cc063b6d600caa38756ff001c58aa7b1928
nickolasnolte1 <61555652+nickolasnolte1@users.noreply.github.com>
2020-09-28 15:33:34 -0600

    main class

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,20 @@
+from ALU import Alu
+from CU import CU
+from Registers import Registers
+from Clock import CPUclock
+from Memory import Memory
+from RAM import Ram
+from ROM import Rom
 
+
+
+class Main:
+
+    def __init__ (self):
+        self.alu = Alu()
+        self.clock = CPUclock()
+        self.memory = Memory()
+        self.ram = Ram()
+        self.rom = Rom()
+        self.registers = Registers()
+        self.cu = CU()
